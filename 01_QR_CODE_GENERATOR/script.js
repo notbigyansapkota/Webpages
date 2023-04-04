ad76b7c7ad001632dbe01b91cd9d53d4ff1d875d
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-04-04 12:42:08 +0545

    Everything done dona done done!

--- 01_QR_CODE_GENERATOR/script.js
+++ 01_QR_CODE_GENERATOR/script.js
@@ -8,9 +8,28 @@ const qrContainer = document.querySelector('.qr-body');
 let size = sizes.value;
 generateBtn.addEventListener('click',(e)=>{
     e.preventDefault();
-    generateQRCode();
+    isEmptyInput();
 });
 
+
+downloadBtn.addEventListener('click', () =>{
+    let img = document.querySelector('.qr-body img');
+    if(img !== null){
+        let imgAttr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAttr);
+    }
+    else{
+        downloadBtn.setAttribute('href', `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
+function isEmptyInput() {
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
+}
+
+sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    generateQRCode();
+});
 function generateQRCode() {
     qrContainer.innerHTML = "";
     new QRCode(qrContainer, {

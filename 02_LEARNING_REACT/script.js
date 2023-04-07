95ef044eb9dd4898f95e13107cebcf0dde820097
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-04-07 22:59:51 +0545

    Learnt Normal and Arrow Function

--- 02_LEARNING_REACT/script.js
+++ 02_LEARNING_REACT/script.js
@@ -1,18 +1,43 @@
-// Learning about let and const
-
-// THIS WORKS BEACAUSE A LET VARIABLE CAN CHANGE ITS VALUE
-/*let myName = 'max';
+// LEARNING ABOUT LET AND CONST---------------------------------
+/*
+// This will work as let variables can change their value.
+let myName = 'max';
+console.log(myName);
+myName = 'Manu';
+console.log(myName)
+// But not this as const variables never change their value.
+const myName = 'Bigyan';
 console.log(myName);
 myName = 'Manu';
 console.log(myName)
 */
 
-// BUT THIS DOESN'T BECAUSE A CONST VARIABLE NEVER CHANGES ITS VALUE
-/*const myName = 'Bigyan';
-console.log(myName);
-myName = 'Manu';
-console.log(myName)
+// NORMAL FUNCTION------------------------------------------
+/*function printMyName (name) {
+    console.log(name);
+}
+printMyName('Bigyan');
 */
+
+// ARROW FUNCTION--------------------------------------------
+/*
+const showName = (name) => {
+    console.log(name);
+}
+showName('Sapkota');
+// using two arguments
+const showFullName = (name, surname) => {
+    return (name + surname);
+};
+console.log(showFullName('Prashant ', 'Ronit'));
+// Let's not use the return keyword
+const noReturnName = (name, surname) => name + surname;
+console.log(noReturnName('Badri ', 'Sapkota'));
+*/
+
+
+
+
 
 
 

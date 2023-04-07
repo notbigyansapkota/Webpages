7119ecd0c59f3a27cc65b71a8935911478d0c894
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-04-07 22:48:50 +0545

    Understood let and const

---
+++ 02_LEARNING_REACT/script.js
@@ -0,0 +1,18 @@
+// Learning about let and const
+
+// THIS WORKS BEACAUSE A LET VARIABLE CAN CHANGE ITS VALUE
+/*let myName = 'max';
+console.log(myName);
+myName = 'Manu';
+console.log(myName)
+*/
+
+// BUT THIS DOESN'T BECAUSE A CONST VARIABLE NEVER CHANGES ITS VALUE
+/*const myName = 'Bigyan';
+console.log(myName);
+myName = 'Manu';
+console.log(myName)
+*/
+
+
+

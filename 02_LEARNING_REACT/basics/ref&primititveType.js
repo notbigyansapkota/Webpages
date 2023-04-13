9806c6dc44dd971270735600a648ece808192d89
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-04-13 19:27:55 +0545

    reference and primitive types

---
+++ 02_LEARNING_REACT/basics/ref&primititveType.js
@@ -0,0 +1,12 @@
+// const number = 69;
+// const num2 = number;
+// console.log(num2);
+
+// const person = {
+//     name: 'Bigyan',
+// }
+// const secondPerson = {
+//     ...person,
+// };
+// person.name = 'Bigyandone';
+// console.log(secondPerson)

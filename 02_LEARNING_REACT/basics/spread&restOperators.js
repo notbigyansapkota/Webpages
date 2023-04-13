cc844076343cac1ea4a6ffb05f432caaa8889bf8
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-04-13 19:17:34 +0545

    learn spread and rest operator

---
+++ 02_LEARNING_REACT/basics/spread&restOperators.js
@@ -0,0 +1,25 @@
+/*
+const numbers = [1, 2, 3];
+// const X = [...y, a, b] ==> Adds a and b on new array X with elements of y
+const newNumbers = [...numbers, 4];
+const test = [numbers, 4]
+console.log(newNumbers);
+console.log(test);
+
+const person = {
+    name: 'Max',
+}
+const newPerson = {
+    ...person,
+    age: 28,
+}
+console.log(newPerson);
+
+const filter = (...args) => {
+    return args.filter(el => el === 1); 
+}
+
+console.log(filter(1, 2, 3, 1.0));
+*/
+
+

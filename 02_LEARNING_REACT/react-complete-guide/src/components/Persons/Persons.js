4c62a5f97c787cfc92c4d1547eccc78ad53f7c04
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-05-22 19:20:57 +0545

    Installing Tailwind XD XD

--- 02_LEARNING_REACT/react-complete-guide/src/components/Persons/Persons.js
+++ 02_LEARNING_REACT/react-complete-guide/src/components/Persons/Persons.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
+const persons = (props) => {
+        console.log('[Person.js] rendering...');
+        return props.persons.map((person, index) => {
         return (
             <Person
               click={() => props.clicked(index)}
@@ -12,6 +14,7 @@ const persons = (props) => props.persons.map((person, index) => {
             />
         );
       })
+    };
 
 
 export default persons;
